88bc3c153f4f8f7fb5c084d742f68a38db27150c
B0user <56859914+B0user@users.noreply.github.com>
2025-05-30 16:25:54 +0500

    back to hidden token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1040,28 +1040,31 @@ export const createInvoiceLink = async (req, res) => {
 
     const planMap = {
       "1_week": {
-        title: "Подписка Премиум",
-        description: "7 дней подписки Премиум",
-        priceRub: 200,
-        amount: 20000,
-        label: "Подписка на неделю",
-        start_parameter: "premium7days"
+      title: "Подписка Премиум",
+      description: "7 дней подписки Премиум",
+      priceRub: 200,
+      amount: 20000,
+      label: "Подписка на неделю",
+      start_parameter: "premium7days",
+      subscription_period: (7*24*3600)
       },
       "1_month": {
-        title: "Подписка Премиум",
-        description: "1 месяц подписки Премиум",
-        priceRub: 500,
-        amount: 50000,
-        label: "Подписка на 1 месяц",
-        start_parameter: "premium1month"
+      title: "Подписка Премиум",
+      description: "1 месяц подписки Премиум",
+      priceRub: 500,
+      amount: 50000,
+      label: "Подписка на 1 месяц",
+      start_parameter: "premium1month",
+      subscription_period: (30*24*3600)
       },
       "3_months": {
-        title: "Подписка Премиум",
-        description: "3 месяца подписки Премиум",
-        priceRub: 1200,
-        amount: 120000,
-        label: "Подписка на 3 месяца",
-        start_parameter: "premium3months"
+      title: "Подписка Премиум",
+      description: "3 месяца подписки Премиум",
+      priceRub: 1200,
+      amount: 120000,
+      label: "Подписка на 3 месяца",
+      start_parameter: "premium3months",
+      subscription_period: (90*24*3600)
       }
     };
 
@@ -1074,36 +1077,38 @@ export const createInvoiceLink = async (req, res) => {
     const result = await axios.post(
       `https://api.telegram.org/bot8193869137:AAFifGJF9t66MPcU5d_DFWvbfAwmufnOhlU/createInvoiceLink`,
       {
-        title: "Подписка Премиум",
-        description: "7 дней подписки Премиум",
-        payload: "premium_7_days",
+        
+        subscription_period: plan.subscription_period,
+        recurring: true,
+        test:true,
+        title: plan.title,
+        description: plan.description,
+        payload: type,
         provider_token: "390540012:LIVE:70096",
         currency: "RUB",
-        prices: [
-          { label: "Подписка на 2 недели", amount: 20000 }
-        ],
+        prices: [{ label: plan.label, amount: plan.amount }],
         need_email: true,
         send_email_to_provider: true,
         need_phone_number: true,
         send_phone_number_to_provider: true,
-        start_parameter: "premium7days",
+        start_parameter: plan.start_parameter,
         provider_data: JSON.stringify({
             receipt: {
-                items: [
-                    {
-                    description: "Подписка на недел.",
-                    quantity: 1,
-                    amount: {
-                        value: 200, // в рублях, не копейках!
-                        currency: "RUB"
-                    },
-                    vat_code: 1,
-                    payment_mode: "full_payment",
-                    payment_subject: "service"
-                    }
-                ],
-                tax_system_code: 1
-            }
+            items: [
+              {
+                description: plan.label,
+                quantity: 1,
+                amount: {
+                  value: plan.priceRub,
+                  currency: "RUB"
+                },
+                vat_code: 1,
+                payment_mode: "full_payment",
+                payment_subject: "service"
+              }
+            ],
+            tax_system_code: 1
+          }
         })
     }
 
